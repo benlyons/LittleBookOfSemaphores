6144462b191579d535c2582a96c532a00ed63d04
Ben <benlyons@hotmail.co.uk>
2018-06-25 23:19:32 +0100

    Test and implement Rendezvous

---
+++ BasicSyncPatterns/Sec03_Rendezvous.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace BasicSyncPatterns
+{
+    public class Sec03_Rendezvous
+    {
+        public enum StatementExecuted
+        {
+            A1,
+            A2,
+            B1,
+            B2
+        }
+
+        public IList<StatementExecuted> StatementsExecuted = new List<StatementExecuted>();
+
+        private readonly Semaphore aArrived;
+
+        private readonly Semaphore bArrived;
+
+        public Sec03_Rendezvous()
+        {
+            this.aArrived = new Semaphore(0, 1);
+            this.bArrived = new Semaphore(0, 1);
+        }
+
+        public void ThreadA()
+        {
+            this.StatementsExecuted.Add(StatementExecuted.A1);
+
+            this.aArrived.Release();
+            this.bArrived.WaitOne();
+
+            this.StatementsExecuted.Add(StatementExecuted.A2);
+        }
+
+        public void ThreadB()
+        {
+            this.StatementsExecuted.Add(StatementExecuted.B1);
+
+            this.bArrived.Release();
+            this.aArrived.WaitOne();
+
+            this.StatementsExecuted.Add(StatementExecuted.B2);
+        }
+    }
+}
---
+++ BasicSyncPatternsTest/TestRendezvous.cs
@@ -0,0 +1,33 @@
+﻿using BasicSyncPatterns;
+using NUnit.Framework;
+using System.Threading;
+using static BasicSyncPatterns.Sec03_Rendezvous;
+
+namespace BasicSyncPatternsTest
+{
+    public class TestRendezvous
+    {
+        [Test]
+        public void Test_Statement_Order()
+        {
+            var test = new Sec03_Rendezvous();
+
+            Thread threadA = new Thread(() => test.ThreadA());
+            Thread threadB = new Thread(() => test.ThreadB());
+
+            threadA.Start();
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+
+            Assert.That(
+                test.StatementsExecuted.IndexOf(StatementExecuted.A1),
+                Is.LessThan(test.StatementsExecuted.IndexOf(StatementExecuted.B2)));
+
+            Assert.That(
+                test.StatementsExecuted.IndexOf(StatementExecuted.B1),
+                Is.LessThan(test.StatementsExecuted.IndexOf(StatementExecuted.A2)));
+        }
+    }
+}

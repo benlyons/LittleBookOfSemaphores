53760db98471f6b9a48e0f24dc335123e819a99c
Ben <benlyons@hotmail.co.uk>
2018-06-26 23:10:42 +0100

    Test Mutex implementation

---
+++ BasicSyncPatterns/Sec04_Mutex.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+
+namespace BasicSyncPatterns
+{
+    public class Sec04_Mutex
+    {
+        private readonly Semaphore Mutex;
+
+        public Sec04_Mutex()
+        {
+            this.Mutex = new Semaphore(1, 1);
+        }
+
+        public int Count { get; private set; } = 0;
+
+        public void ThreadA()
+        {
+            this.Mutex.WaitOne();
+
+            this.Count = this.Count + 1;
+
+            this.Mutex.Release();
+        }
+
+        public void ThreadB()
+        {
+            this.Mutex.WaitOne();
+
+            this.Count = this.Count + 1;
+
+            this.Mutex.Release();
+        }
+    }
+}
---
+++ BasicSyncPatternsTest/TestMutex.cs
@@ -0,0 +1,30 @@
+﻿using BasicSyncPatterns;
+using NUnit.Framework;
+using System.Threading;
+
+namespace BasicSyncPatternsTest
+{
+    public class TestMutex
+    {
+        [Test]
+        public void Test_TwoThreadsIncrementCount_CountIsIncrementedCorrectly()
+        {
+            // I've tried running this this test against code without a mutex 100,000 times in a loop.
+            // It almost never goes wrong.
+            // Strangely, the only times it did go wrong, it went wrong on the first iteration of the loop.
+            // Overall, it's extremely hard to repeat.
+            var test = new Sec04_Mutex();
+
+            Thread threadA = new Thread(() => test.ThreadA());
+            Thread threadB = new Thread(() => test.ThreadB());
+
+            threadA.Start();
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+
+            Assert.That(test.Count, Is.EqualTo(2));
+        }
+    }
+}

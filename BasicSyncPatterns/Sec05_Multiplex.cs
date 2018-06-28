6bd1088bae7ce655b616f33600603bae2fbf007c
Ben <benlyons@hotmail.co.uk>
2018-06-28 23:04:02 +0100

    Implement and test multiplex

---
+++ BasicSyncPatterns/Sec05_Multiplex.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace BasicSyncPatterns
+{
+    public class Sec05_Multiplex
+    {
+        private readonly Semaphore multiplex;
+
+        private readonly Semaphore mutex;
+
+        private int threadsInCriticalSection;
+
+        public Sec05_Multiplex(int maxThreadsInCriticalSection)
+        {
+            this.PeakThreadsInCriticalSection = 0;
+            this.multiplex = new Semaphore(5, maxThreadsInCriticalSection);
+
+            // This mutex isn't really part of this demonstration, but is necessary in order to 
+            // make this code testable. I want to keep track of how many threads are in the critical
+            // section, and in order to do that in a thread-safe way I need to make sure that only
+            // one of the threads in the critical section is accessing that count!
+            this.mutex = new Semaphore(1, 1);
+        }
+
+        public int PeakThreadsInCriticalSection { get; private set; }
+
+        public void RunCode()
+        {
+            this.RunCriticalSection();
+        }
+
+        private void RunCriticalSection()
+        {
+            this.multiplex.WaitOne();
+
+            this.mutex.WaitOne();
+            this.threadsInCriticalSection++;
+            this.mutex.Release();
+
+            this.PeakThreadsInCriticalSection = Math.Max(this.PeakThreadsInCriticalSection, this.threadsInCriticalSection);
+
+            // Simulate doing some stuff
+            Thread.Sleep(5);
+
+            this.mutex.WaitOne();
+            this.threadsInCriticalSection--;
+            this.mutex.Release();
+
+            this.multiplex.Release();
+        }
+    }
+}
---
+++ BasicSyncPatternsTest/Test05_Multiplex.cs
@@ -0,0 +1,44 @@
+﻿using BasicSyncPatterns;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace BasicSyncPatternsTest
+{
+    public class Test05_Multiplex
+    {
+        [Test]
+        public void Test_LimitNumberOfThreadsInCriticalSection()
+        {
+            int maxThreadsInCriticalSection = 5;
+
+            var test = new Sec05_Multiplex(maxThreadsInCriticalSection);
+
+            IList<Thread> threads = new List<Thread>();
+
+            int numberOfThreadsToRUn = 10;
+
+            for (int i = 0; i < numberOfThreadsToRUn; i++)
+            {
+                var thread = new Thread(() => test.RunCode());
+
+                threads.Add(thread);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            Assert.That(
+                test.PeakThreadsInCriticalSection, 
+                Is.LessThanOrEqualTo(maxThreadsInCriticalSection),
+                message: $"Peak threads in critical section was: {test.PeakThreadsInCriticalSection}");
+        }
+    }
+}

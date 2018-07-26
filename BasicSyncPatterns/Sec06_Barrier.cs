eac3e00324e61fb68aff0e41846d7eaa69de24a3
Ben <benlyons@hotmail.co.uk>
2018-07-26 22:52:31 +0100

    Testing a barrier for any number of threads

---
+++ BasicSyncPatterns/Sec06_Barrier.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace BasicSyncPatterns
+{
+    public class Sec06_Barrier
+    {
+        public enum StatementExecuted
+        {
+            Rendezvous,
+            CriticalPoint
+        }
+
+        public Sec06_Barrier(int maxThreadCount)
+        {
+            this.n = 0;
+            this.threadCount = maxThreadCount;
+
+            this.mutex = new Semaphore(1, 1);
+            this.barrier = new Semaphore(0, 1);
+        }
+
+        public static IList<StatementExecuted> StatementsExecuted = new List<StatementExecuted>();
+        private int n;
+        private readonly int threadCount;
+        private readonly Semaphore mutex;
+        private readonly Semaphore barrier;
+
+        public void RunCode()
+        {
+            StatementsExecuted.Add(StatementExecuted.Rendezvous);
+
+            this.mutex.WaitOne();
+            this.n++;
+            this.mutex.Release();
+
+            if(this.threadCount == this.n)
+            {
+                this.barrier.Release();
+            }
+
+            this.barrier.WaitOne();
+            this.barrier.Release();
+
+            StatementsExecuted.Add(StatementExecuted.CriticalPoint);
+        }
+    }
+}
---
+++ BasicSyncPatternsTest/Test06_Barrier.cs
@@ -0,0 +1,42 @@
+﻿using BasicSyncPatterns;
+using NUnit.Framework;
+using System.Linq;
+using System.Threading;
+using static BasicSyncPatterns.Sec06_Barrier;
+
+namespace BasicSyncPatternsTest
+{
+    public class Test06_Barrier
+    {
+        [Test]
+        public void TestSingleThreaded()
+        {
+            var test = new Sec06_Barrier(1);
+
+            test.RunCode();
+
+            Assert.That(Sec06_Barrier.StatementsExecuted[0], Is.EqualTo(StatementExecuted.Rendezvous));
+            Assert.That(Sec06_Barrier.StatementsExecuted[1], Is.EqualTo(StatementExecuted.CriticalPoint));
+        }
+
+        [Test]
+        public void TestMultiThreaded()
+        {
+            int threadCount = 25;
+
+            var test = new Sec06_Barrier(threadCount);
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                Thread thread = new Thread(() => test.RunCode());
+                thread.Start();
+            }
+
+            var expectedStatementsExecuted =
+                Enumerable.Repeat(StatementExecuted.Rendezvous, threadCount).Concat(
+                Enumerable.Repeat(StatementExecuted.CriticalPoint, threadCount));
+
+            CollectionAssert.AreEqual(expectedStatementsExecuted, Sec06_Barrier.StatementsExecuted);
+        }
+    }
+}
